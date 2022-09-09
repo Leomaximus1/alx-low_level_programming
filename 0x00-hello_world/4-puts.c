7bbda25523ce2f74eea9286650b5029c2d843b6e
Leo <sumixamoel@gmail.com>
2022-09-09 07:46:48 -0700

    hotfix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 #include <string.h>
 /**
  *main - Entry point
- *
  *Description - C program that prints exactly "Programming 
  * is like building a multilingual puzzzle \n"
- *
  *Return: Always 0 (Success)
  */
 int main(void)
