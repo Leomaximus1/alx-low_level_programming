422e1612a4c42c72710d1dba0da383333e4993bd
Leo <sumixamoel@gmail.com>
2022-09-09 07:40:03 -0700

    hotfix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,17 @@
 #include <string.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Description - C program that prints exactly "Programming 
+ *Description - C program that prints exactly "Programming 
  * is like building a multilingual puzzzle \n"
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str1[54];
-	strcpy(str1,"\"Programming is like building a multilingual puzzle");
-	puts(str1);
-	return(0);
+char str1[54];
+strcpy (str1, "\"Programming is like building a multilingual puzzle");
+puts(str1);
+return(0);
 }
-  
