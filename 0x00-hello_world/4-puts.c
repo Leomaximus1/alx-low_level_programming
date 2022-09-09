9875721866a4a140c5732d89bac897b5fc219ffc
Leo <sumixamoel@gmail.com>
2022-09-09 07:36:07 -0700

    hotfix on last commit for Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+
 /**
- * main - Entry point -
+ * main - Entry point
+ *
  * Description - C program that prints exactly "Programming 
  * is like building a multilingual puzzzle \n"
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-char str1[54];
-strcpy(str1,"\"Programming is like building a multilingual puzzle \n");
-puts(str1);
-return(0);
+	char str1[54];
+	strcpy(str1,"\"Programming is like building a multilingual puzzle");
+	puts(str1);
+	return(0);
 }
   
